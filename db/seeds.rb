7a434da08f46a3e29d84be346eb068956e08d4e1
github username <jrgreiner115@gmail.com>
2018-05-30 11:36:13 -0400

    users api works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+joe = User.create(name: "JRG")
+brie = User.create(name: "BRS")
+roger = User.create(name: "ROG")
+
+score1 = Score.create(score: 1000, user_id: 1)
+score2 = Score.create(score: 2000, user_id: 2)
+score3 = Score.create(score: 3000, user_id: 3)
